bc38e90cecc0e16c7b4acc25aec4819ee72f0f49
Przemysław Dominikowski <57873253+lavar3l@users.noreply.github.com>
2020-03-16 10:50:41 +0100

    Bar prototype

--- maintenance-mode-plugin/inc/Base/AdminBar.php
+++ maintenance-mode-plugin/inc/Base/AdminBar.php
@@ -20,12 +20,36 @@ class AdminBar extends BaseController
         global $wp_admin_bar;
 
         $isEnabled = (get_option($this->prefix.'enabled') ? 'ON' : 'OFF');
+        $isEnabled = get_option($this->prefix.'enabled');
+
+        if ($isEnabled)
+        {
+            $action = '<input type="checkbox" style="display: none" class="tgl tgl-flat" id="MMP_enabled" name="MMP_enabled" checked><label class="tgl-btn" for="MMP_enabled"></label>';
+            $isEnabledLabel = '<span style="color:green">⬤</span>';
+        }
+        else
+        {
+            $action = '<input type="checkbox" style="display: none" class="tgl tgl-flat" id="MMP_enabled" name="MMP_enabled"><label class="tgl-btn" for="MMP_enabled"></label>';
+            $isEnabledLabel = '<span style="color:red">⭕</span>';
+        }
 
         $wp_admin_bar->add_menu([
             'parent' => '',
             'id'     => $this->prefix.'bar',
-            'title'  => 'Maintenance Mode: '.$isEnabled,
+            'title'  => 'Maintenance Mode: '.$isEnabledLabel,
             'href'   => admin_url('options-general.php?page='.$this->pageName)
         ]);
+        $wp_admin_bar->add_node( array(
+            'parent' => $this->prefix.'bar',
+            'id'    => $this->prefix.'state',
+            'title' => $action,
+            'href'  => false,
+        ));
+        $wp_admin_bar->add_node( array(
+            'parent' => $this->prefix.'bar',
+            'id'     => $this->prefix.'settings',
+            'title'  => 'Settings',
+            'href'   => admin_url('options-general.php?page='.$this->pageName)
+        ));
     }
 }
