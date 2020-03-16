db7b02254abbcd1185f1abe7169c65d85c11a1e1
Tymoteusz Bartnik <battnik90@gmail.com>
2020-03-16 10:55:51 +0100

    Fix admin bar

--- maintenance-mode-plugin/inc/Base/AdminBar.php
+++ maintenance-mode-plugin/inc/Base/AdminBar.php
@@ -19,17 +19,16 @@ class AdminBar extends BaseController
     {
         global $wp_admin_bar;
 
-        $isEnabled = (get_option($this->prefix.'enabled') ? 'ON' : 'OFF');
         $isEnabled = get_option($this->prefix.'enabled');
 
         if ($isEnabled)
         {
-            $action = '<input type="checkbox" style="display: none" class="tgl tgl-flat" id="MMP_enabled" name="MMP_enabled" checked><label class="tgl-btn" for="MMP_enabled"></label>';
+            //$action = '<input type="checkbox" style="display: none" class="tgl tgl-flat" id="MMP_enabled" name="MMP_enabled" checked><label class="tgl-btn" for="MMP_enabled"></label>';
             $isEnabledLabel = '<span style="color:green">⬤</span>';
         }
         else
         {
-            $action = '<input type="checkbox" style="display: none" class="tgl tgl-flat" id="MMP_enabled" name="MMP_enabled"><label class="tgl-btn" for="MMP_enabled"></label>';
+            //$action = '<input type="checkbox" style="display: none" class="tgl tgl-flat" id="MMP_enabled" name="MMP_enabled"><label class="tgl-btn" for="MMP_enabled"></label>';
             $isEnabledLabel = '<span style="color:red">⭕</span>';
         }
 
